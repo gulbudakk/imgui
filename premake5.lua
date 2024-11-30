a2c3df1fc23a1b2bd8391f741fdf03f6b573f31d
Kalender Gülbudak <58489995+gulbudakk@users.noreply.github.com>
2024-11-30 14:10:45 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "ImGui"
+  kind "StaticLib"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objsdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files 
+  {
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp",
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++20"
+    staticruntime "On"
+
+  filet {"system:windows", "configurations:Release"}
+    buildoptions "/MT"
